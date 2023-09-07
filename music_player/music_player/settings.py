f1c5b201327a424dc883f7bbe895cf10b486eda4
huongnt-2545 <49477980+huongnt-2545@users.noreply.github.com>
2023-09-07 14:27:55 +0700

    Homepage

--- music_player/music_player/settings.py
+++ music_player/music_player/settings.py
@@ -60,7 +60,7 @@ AUTH_USER_MODEL = "catalog.User"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')] ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
